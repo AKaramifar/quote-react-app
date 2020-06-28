4d041dbbaf17e66baa192f1c6005fa3266519ae0
Afshin <47743758+AKaramifar@users.noreply.github.com>
2020-06-28 01:12:47 +0100

    update

--- src/componenets/Header.js
+++ src/componenets/Header.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import "./Header.css";
 
-const Header = () => {
+const Header = ({fetchQoutes_F}) => {
     const [qoute, setQoute] = useState([]);
     const fetchQoutes = () =>{
         fetch('https://akaramifar-quote-server.herokuapp.com/quotes/random')
@@ -18,12 +18,12 @@ const Header = () => {
     return(
         <div className="Div_Header_Style">
             <div className="Div_MainQoute_Style">
-                <div className="Div_Qoute_Style"><i class="I_Qoute_Style fas fa-quote-left"></i>{qoute.Quote || "I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed."}</div>
+                <div className="Div_Qoute_Style"><i className="I_Qoute_Style fas fa-quote-left"></i>{qoute.Quote || "I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed."}</div>
                 <div className="Div_Author_Style">{qoute.author || "Michael Jordan"}</div>
             </div>
             <div className="Div_Search_Style">
-                <input type="text" className="Input_Search_Style"></input>
-                <p className="P_Search_Style">Search</p>
+                <input id="Input_Search_JSX" type="text" className="Input_Search_Style" placeholder="Search your Keyword"></input>
+                <p className="P_Search_Style" onClick={() => fetchQoutes_F()}>Search</p>
             </div>
         </div>
     )
